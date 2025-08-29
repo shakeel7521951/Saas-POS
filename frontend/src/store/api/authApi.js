89d2bf06e928ec8a777daf94ef7ea67de7850691
Mogehs <mugheesurrehman06@gmail.com>
2025-08-29 14:12:32 +0500

    base url

--- frontend/src/store/api/authApi.js
+++ frontend/src/store/api/authApi.js
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api`;
 
 const baseQuery = fetchBaseQuery({
   baseUrl: API_BASE_URL,
